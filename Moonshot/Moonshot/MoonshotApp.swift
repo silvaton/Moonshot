d57279449e5fe423eb4cfe7833a3a10a929ec551
silvaton <tdasilva@ext.laligatech.com>
2023-06-14 12:45:59 +0200

    Day-040: Added json files

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ton Silva on 14/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
